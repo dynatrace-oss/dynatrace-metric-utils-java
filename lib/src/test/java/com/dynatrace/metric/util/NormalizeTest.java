d612548e7f888c25de9be8ec4d2e122a4ae2bb98
Georg <georg.pirklbauer@dynatrace.com>
2021-03-18 17:58:40 +0100

    fix testing framework to run parameterized tests

--- lib/src/test/java/com/dynatrace/metric/util/NormalizeTest.java
+++ lib/src/test/java/com/dynatrace/metric/util/NormalizeTest.java
@@ -14,18 +14,19 @@
 package com.dynatrace.metric.util;
 
 import java.util.stream.Stream;
-import junit.framework.TestCase;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class NormalizeTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NormalizeTest  {
 
   public void testDimensionList() {}
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "{index}: {0}, input: {1}, expected: {2}")
   @MethodSource("provideDimensionKeys")
-  public void testDimensionKey(String input, String expected) {
+  public void testDimensionKey(String name, String input, String expected) {
     assertEquals(expected, Normalize.dimensionKey(input));
   }
 
@@ -34,6 +35,6 @@ public class NormalizeTest extends TestCase {
   public void testMetricKey() {}
 
   private static Stream<Arguments> provideDimensionKeys() {
-    return Stream.of(Arguments.of("key1", "key1"));
+    return Stream.of(Arguments.of("mytest", "key1", "key1"));
   }
 }
