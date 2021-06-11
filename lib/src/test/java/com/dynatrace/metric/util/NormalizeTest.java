481eeca64d276bb124ccbe98e016863cbf81d63f
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-06-11 10:53:19 +0200

    update the normalization logic to follow the new logic

--- lib/src/test/java/com/dynatrace/metric/util/NormalizeTest.java
+++ lib/src/test/java/com/dynatrace/metric/util/NormalizeTest.java
@@ -13,14 +13,15 @@
  */
 package com.dynatrace.metric.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.Instant;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.Instant;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NormalizeTest {
 
