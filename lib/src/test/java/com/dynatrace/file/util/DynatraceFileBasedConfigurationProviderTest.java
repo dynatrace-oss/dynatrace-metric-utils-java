540c2a42e0ed5a847378dae05f42ffdd717d7e88
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2022-02-18 14:25:54 +0100

    update and unflake tests

--- lib/src/test/java/com/dynatrace/file/util/DynatraceFileBasedConfigurationProviderTest.java
+++ lib/src/test/java/com/dynatrace/file/util/DynatraceFileBasedConfigurationProviderTest.java
@@ -1,23 +1,41 @@
 package com.dynatrace.file.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.dynatrace.metric.util.DynatraceMetricApiConstants;
 import com.dynatrace.testutils.TestUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DynatraceFileBasedConfigurationProviderTest {
   // We rely on the fact that JUnit runs tests in series, not parallel.
+
+  static Path tempDir;
+
+  @BeforeAll
+  static void setUpClass() throws IOException {
+    tempDir = Files.createTempDirectory("tempdir");
+  }
+
+  @AfterAll
+  static void tearDown() throws IOException {
+    Files.deleteIfExists(tempDir);
+  }
 
   @Test
   void testNonExistentFileReturnsDefaults() {
     final DynatraceFileBasedConfigurationProvider instance =
         DynatraceFileBasedConfigurationProvider.getInstance();
     // Set up test
-    instance.forceOverwriteConfig(TestUtils.generateNonExistentFilename());
+    instance.forceOverwriteConfig(TestUtils.generateNonExistentFilename(), Duration.ofMillis(500));
 
     assertEquals(
         DynatraceMetricApiConstants.getDefaultOneAgentEndpoint(),
@@ -30,7 +48,8 @@ class DynatraceFileBasedConfigurationProviderTest {
     final DynatraceFileBasedConfigurationProvider instance =
         DynatraceFileBasedConfigurationProvider.getInstance();
     // Set up test
-    instance.forceOverwriteConfig("src/test/resources/config_invalid.properties");
+    instance.forceOverwriteConfig(
+        "src/test/resources/config_invalid.properties", Duration.ofMillis(500));
 
     assertEquals(
         DynatraceMetricApiConstants.getDefaultOneAgentEndpoint(),
@@ -43,7 +62,15 @@ class DynatraceFileBasedConfigurationProviderTest {
     final DynatraceFileBasedConfigurationProvider instance =
         DynatraceFileBasedConfigurationProvider.getInstance();
     // Set up test
-    instance.forceOverwriteConfig("src/test/resources/config_valid.properties");
+    instance.forceOverwriteConfig(
+        "src/test/resources/config_valid.properties", Duration.ofMillis(50));
+
+    await()
+        .atMost(150, TimeUnit.MILLISECONDS)
+        .until(
+            () ->
+                "https://your-dynatrace-ingest-url/api/v2/metrics/ingest"
+                    .equals(instance.getMetricIngestEndpoint()));
 
     assertEquals(
         "https://your-dynatrace-ingest-url/api/v2/metrics/ingest",
@@ -53,18 +80,18 @@ class DynatraceFileBasedConfigurationProviderTest {
 
   @Test
   void testConfigIsUpdatedIfFileChanges() throws IOException, InterruptedException {
-    final Path tempfile = Files.createTempFile("tempfile", ".properties");
+    final Path tempfile = Files.createTempFile(tempDir, "tempfile", ".properties");
     Files.write(
         tempfile,
         ("DT_METRICS_INGEST_URL = original_url\n" + "DT_METRICS_INGEST_API_TOKEN = original_token")
             .getBytes());
     // wait for the nonblocking io to finish writing.
-    Thread.sleep(10);
+    Thread.sleep(30);
 
     final DynatraceFileBasedConfigurationProvider instance =
         DynatraceFileBasedConfigurationProvider.getInstance();
     // Set up test
-    instance.forceOverwriteConfig(tempfile.toString());
+    instance.forceOverwriteConfig(tempfile.toString(), Duration.ofMillis(50));
 
     assertEquals("original_url", instance.getMetricIngestEndpoint());
     assertEquals("original_token", instance.getMetricIngestToken());
@@ -73,10 +100,13 @@ class DynatraceFileBasedConfigurationProviderTest {
         tempfile,
         ("DT_METRICS_INGEST_URL = new_url\n" + "DT_METRICS_INGEST_API_TOKEN = new_token")
             .getBytes());
-    // wait for nonblocking IO
-    Thread.sleep(10);
 
+    // wait for nonblocking IO
+    await()
+        .atMost(150, TimeUnit.MILLISECONDS)
+        .until(() -> instance.getMetricIngestEndpoint().equals("new_url"));
     assertEquals("new_url", instance.getMetricIngestEndpoint());
     assertEquals("new_token", instance.getMetricIngestToken());
+    Files.delete(tempfile);
   }
 }
--- lib/src/test/java/com/dynatrace/file/util/FilePollerTest.java
+++ lib/src/test/java/com/dynatrace/file/util/FilePollerTest.java
@@ -1,24 +1,71 @@
 package com.dynatrace.file.util;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.dynatrace.testutils.TestUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.dynatrace.file.util.FilePoller.FilePollerKind.POLL_BASED;
+import static com.dynatrace.file.util.FilePoller.FilePollerKind.WATCHSERVICE_BASED;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FilePollerTest {
+  Path tempDir;
+  List<Path> tempFiles = new ArrayList<>();
+
+  boolean isMacOs = System.getProperty("os.name").toLowerCase().contains("mac");
+
+  @BeforeEach
+  void setUp() throws IOException {
+    cleanUp();
+    tempDir = Files.createTempDirectory("tempdir");
+    for (int i = 0; i < 2; i++) {
+      tempFiles.add(Files.createTempFile(tempDir, "tempfile" + i, ".tmp"));
+    }
+  }
+
+  @AfterEach
+  void cleanUp() throws IOException {
+    for (Path path : tempFiles) {
+      Files.deleteIfExists(path);
+    }
+    tempFiles.clear();
+    if (tempDir != null) {
+      Files.deleteIfExists(tempDir);
+    }
+    tempDir = null;
+  }
+
   @Test
   void filePollerConstructorThrowsOnNonExistentFile() {
     final String nonExistentFile = TestUtils.generateNonExistentFilename();
 
     assertThrows(
         IllegalArgumentException.class,
-        () -> {
-          new FilePoller(nonExistentFile);
-        });
+        () -> new FilePoller(nonExistentFile, POLL_BASED, Duration.ofMillis(50)));
+
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new FilePoller(nonExistentFile, WATCHSERVICE_BASED, Duration.ofMillis(50)));
+  }
+
+  @Test
+  void constructorThrowsOnNullDurationForPollBased() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new FilePoller(tempFiles.get(0).toString(), POLL_BASED, null));
+    // ignored for WatchService based impl.
+    assertDoesNotThrow(() -> new FilePoller(tempFiles.get(0).toString(), WATCHSERVICE_BASED, null));
   }
 
   @Test
@@ -27,32 +74,47 @@ class FilePollerTest {
 
     assertThrows(
         IllegalArgumentException.class,
-        () -> {
-          new FilePoller(tempDir.toString());
-        });
+        () -> new FilePoller(tempDir.toString(), POLL_BASED, Duration.ofMillis(50)));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new FilePoller(tempDir.toString(), WATCHSERVICE_BASED, Duration.ofMillis(50)));
   }
 
-  @Test
-  void filePollerUpdatesOnChange() throws IOException, InterruptedException {
-    final Path tempFile = Files.createTempFile("tempfile", ".txt");
+  void filePollerUpdatesOnChange(FilePoller poller) throws IOException {
+    final Path tempFile = tempFiles.get(0);
 
-    final FilePoller poller = new FilePoller(tempFile.toString());
     assertFalse(poller.fileContentsUpdated());
 
     Files.write(tempFile, "test file content".getBytes());
-    // wait for non-blocking IO
-    Thread.sleep(10);
 
-    assertTrue(poller.fileContentsUpdated());
+    // wait for non-blocking IO
+    await().atMost(1, TimeUnit.SECONDS).until(poller::fileContentsUpdated);
     assertFalse(poller.fileContentsUpdated());
   }
 
   @Test
-  void filePollerUpdatesOnFileMove() throws IOException, InterruptedException {
-    final Path tempFile1 = Files.createTempFile("tempfile", ".txt");
-    final Path tempFile2 = Files.createTempFile("tempfile", ".txt");
+  void filePollerUpdatesOnChangePollBased() throws IOException {
+    try (FilePoller poller =
+        new FilePoller(tempFiles.get(0).toString(), POLL_BASED, Duration.ofMillis(50))) {
+      filePollerUpdatesOnChange(poller);
+    }
+  }
 
-    final FilePoller poller = new FilePoller(tempFile1.toString());
+  @Test
+  void filePollerUpdatesOnChangeWatchServiceBased() throws IOException {
+    if (isMacOs) {
+      System.out.println("Running on macOS, skipping this test...");
+      return;
+    }
+
+    try (FilePoller poller =
+        new FilePoller(tempFiles.get(0).toString(), WATCHSERVICE_BASED, null)) {
+      filePollerUpdatesOnChange(poller);
+    }
+  }
+
+  void filePollerUpdatesOnFileMove(FilePoller poller, Path tempFile1, Path tempFile2)
+      throws IOException {
     // set up the second file
     Files.write(tempFile2, "test file content for tempFile2".getBytes());
 
@@ -61,28 +123,76 @@ class FilePollerTest {
 
     // move the second file into position.
     Files.move(tempFile2, tempFile1, REPLACE_EXISTING);
-    // wait for non-blocking io to be finished.
-    Thread.sleep(10);
 
-    assertTrue(poller.fileContentsUpdated());
+    // wait for non-blocking io to be finished.
+    await().atMost(1, TimeUnit.SECONDS).until(poller::fileContentsUpdated);
     assertFalse(poller.fileContentsUpdated());
   }
 
   @Test
-  void filePollerUpdatesOnFileCopy() throws IOException, InterruptedException {
-    final Path tempFile1 = Files.createTempFile("tempfile", ".txt");
-    final Path tempFile2 = Files.createTempFile("tempfile", ".txt");
+  void filePollerUpdatesOnFileMovePollBased() throws IOException {
+    final Path tempFile1 = tempFiles.get(0);
+    final Path tempFile2 = tempFiles.get(1);
 
-    final FilePoller poller = new FilePoller(tempFile1.toString());
+    try (FilePoller poller =
+        new FilePoller(tempFile1.toString(), POLL_BASED, Duration.ofMillis(50))) {
+      filePollerUpdatesOnFileMove(poller, tempFile1, tempFile2);
+    }
+  }
+
+  @Test
+  void filePollerUpdatesOnFileMoveWatchServiceBased() throws IOException {
+    if (isMacOs) {
+      System.out.println("Running on macOS, skipping this test...");
+      return;
+    }
+
+    final Path tempFile1 = tempFiles.get(0);
+    final Path tempFile2 = tempFiles.get(1);
+
+    try (FilePoller poller =
+        new FilePoller(tempFile1.toString(), WATCHSERVICE_BASED, null)) {
+      filePollerUpdatesOnFileMove(poller, tempFile1, tempFile2);
+    }
+  }
+
+  void filePollerUpdatesOnFileCopy(FilePoller poller, Path tempFile1, Path tempFile2)
+      throws IOException {
     Files.write(tempFile2, "test file content for tempFile2".getBytes());
 
     assertFalse(poller.fileContentsUpdated());
 
     Files.copy(tempFile2, tempFile1, REPLACE_EXISTING);
-    // wait for non-blocking io
-    Thread.sleep(10);
 
-    assertTrue(poller.fileContentsUpdated());
+    // wait for non-blocking IO
+    await().atMost(1, TimeUnit.SECONDS).until(poller::fileContentsUpdated);
     assertFalse(poller.fileContentsUpdated());
+  }
+
+  @Test
+  void filePollerUpdatesOnFileCopyPollBased() throws IOException {
+    final Path tempFile1 = tempFiles.get(0);
+    final Path tempFile2 = tempFiles.get(1);
+
+    try (FilePoller poller =
+        new FilePoller(tempFile1.toString(), POLL_BASED, Duration.ofMillis(50))) {
+      filePollerUpdatesOnFileCopy(poller, tempFile1, tempFile2);
+    }
+  }
+
+  @Test
+  void filePollerUpdatesOnFileCopyWatchServiceBased() throws IOException {
+    if (isMacOs) {
+      System.out.println("Running on macOS, skipping this test...");
+      return;
+    }
+
+    final Path tempFile1 = tempFiles.get(0);
+    final Path tempFile2 = tempFiles.get(1);
+
+    try (FilePoller poller =
+        new FilePoller(tempFile1.toString(), WATCHSERVICE_BASED, null)) {
+      filePollerUpdatesOnFileCopy(poller, tempFile1, tempFile2);
+    }
   }
 }
