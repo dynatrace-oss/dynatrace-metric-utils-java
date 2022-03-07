efd4a0334dbe7856955f6647713f1fd183374a67
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2022-03-07 16:33:38 +0100

    simplify hash creation and storage

--- lib/src/main/java/com/dynatrace/file/util/PollBasedFilePoller.java
+++ lib/src/main/java/com/dynatrace/file/util/PollBasedFilePoller.java
@@ -13,24 +13,23 @@
  */
 package com.dynatrace.file.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 class PollBasedFilePoller extends FilePoller {
-  private final AtomicBoolean changedSinceLastInquiry = new AtomicBoolean(false);
-  private static final Logger logger = Logger.getLogger(PollBasedFilePoller.class.getName());
+  private static final Logger LOGGER = Logger.getLogger(PollBasedFilePoller.class.getName());
+  private static final String LOG_MESSAGE_FAILED_FILE_READ = "Failed to read file %s. Error: %s";
 
-  private long prevModMillis = 0;
-  private String prevChecksum = "";
+  private final AtomicBoolean changedSinceLastInquiry = new AtomicBoolean(false);
+  private byte[] prevChecksumBytes = null;
 
   private final ScheduledFuture<?> worker;
   private final ScheduledExecutorService executorService;
@@ -60,7 +59,7 @@ class PollBasedFilePoller extends FilePoller {
               }
             });
 
-    logger.finer(() -> String.format("Polling every %dms", pollInterval.toMillis()));
+    LOGGER.finer(() -> String.format("Polling every %dms", pollInterval.toMillis()));
     worker =
         executorService.scheduleAtFixedRate(
             this::poll, pollInterval.toNanos(), pollInterval.toNanos(), TimeUnit.NANOSECONDS);
@@ -76,72 +75,41 @@ class PollBasedFilePoller extends FilePoller {
 
   private synchronized void initialPoll() {
     try {
-      prevChecksum = getFileChecksum();
-      prevModMillis =
-          Files.getLastModifiedTime(absoluteFilePath, LinkOption.NOFOLLOW_LINKS).toMillis();
+      prevChecksumBytes = getChecksumBytes();
     } catch (IOException e) {
-      logger.warning(
-          () ->
-              String.format(
-                  "Failed to read file %s, stopping polling. Error: %s", absoluteFilePath, e));
-      worker.cancel(true);
+      LOGGER.warning(() -> String.format(LOG_MESSAGE_FAILED_FILE_READ, absoluteFilePath, e));
     }
   }
 
   private synchronized void poll() {
     try {
-      // check if the filetime has changed.
-      long lastModifiedMillis = Files.getLastModifiedTime(absoluteFilePath).toMillis();
-      if (lastModifiedMillis > prevModMillis) {
-        prevModMillis = lastModifiedMillis;
-        changedSinceLastInquiry.set(true);
-        return;
-      }
+      byte[] latestChecksumBytes = getChecksumBytes();
 
-      // if the filetime hasn't changed, check if the hash has changed.
-      String fileChecksum = getFileChecksum();
-      if (!fileChecksum.equals(prevChecksum)) {
-        prevChecksum = fileChecksum;
-        prevModMillis = lastModifiedMillis;
-        changedSinceLastInquiry.set(true);
+      if (!Arrays.equals(latestChecksumBytes, prevChecksumBytes)) {
+        prevChecksumBytes = latestChecksumBytes;
+
+        // The file did exist before (prevChecksumBytes != null) but doesn't anymore
+        // (latestChecksumBytes == null).
+        // This code here is only reached when prevChecksumBytes != latestChecksumBytes.
+        // This means the file has been deleted since the last poll, which should not trigger a
+        // state change.
+        if (latestChecksumBytes != null) {
+          changedSinceLastInquiry.set(true);
+        }
       }
     } catch (IOException e) {
-      // One possible reason for this is that no read permissions exist on the file, in
-      // which case the user should (try to) read the file anyway and handle any errors then.
-      changedSinceLastInquiry.set(true);
-      logger.warning(
-          () ->
-              String.format(
-                  "Failed to read file %s, stopping polling. Error: %s", absoluteFilePath, e));
-      worker.cancel(true);
+      LOGGER.warning(() -> String.format(LOG_MESSAGE_FAILED_FILE_READ, absoluteFilePath, e));
     }
   }
 
-  private synchronized String getFileChecksum() throws IOException {
-    // Get file input stream for reading the file content
-    try (FileInputStream fis = new FileInputStream(absoluteFilePath.toFile())) {
-
-      // Create byte array to read data in chunks
-      byte[] byteArray = new byte[1024];
-      int bytesCount;
-
-      // Read file data and update in message digest
-      while ((bytesCount = fis.read(byteArray)) != -1) {
-        md5.update(byteArray, 0, bytesCount);
-      }
+  private synchronized byte[] getChecksumBytes() throws IOException {
+    byte[] bytes;
+    try {
+      bytes = md5.digest(Files.readAllBytes(absoluteFilePath));
+    } finally {
+      md5.reset();
     }
-
-    // Get the hash's bytes
-    byte[] bytes = md5.digest();
-
-    // This bytes[] has bytes in decimal format, convert it to hexadecimal format
-    StringBuilder sb = new StringBuilder();
-    for (byte aByte : bytes) {
-      sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
-    }
-
-    // return complete hash
-    return sb.toString();
+    return bytes;
   }
 
   @Override
@@ -153,7 +121,7 @@ class PollBasedFilePoller extends FilePoller {
         executorService.shutdownNow();
       }
     } catch (InterruptedException e) {
-      logger.warning("failed to shut down poll based file poller: " + e);
+      LOGGER.warning("failed to shut down poll based file poller: " + e);
     }
   }
 }
