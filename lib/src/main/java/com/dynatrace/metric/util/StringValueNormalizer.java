f7a98f33f39d2436f2603c8d688de351573c7b93
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2023-07-13 14:53:01 +0200

    improve wording

--- lib/src/main/java/com/dynatrace/metric/util/StringValueNormalizer.java
+++ lib/src/main/java/com/dynatrace/metric/util/StringValueNormalizer.java
@@ -8,10 +8,9 @@ class StringValueNormalizer {
   private StringValueNormalizer() {}
 
   /**
-   * Applies normalization to the string. Normalizations applied:
-   * * Replaces invalid characters with underscores
-   * * Escapes control characters with a backslash
-   * * Truncates strings that exceed the line length limit
+   * Applies normalization to the string. Normalizations applied: Replaces invalid characters with
+   * underscores, escapes control characters with a backslash, and truncates strings that exceed the
+   * line length limit
    *
    * @param value The string to normalize.
    * @param maxStringValueLength The maximum length of the string.
