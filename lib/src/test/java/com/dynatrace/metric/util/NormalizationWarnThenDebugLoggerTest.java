2b2be8905cab9504f6c50feecd0b610984bdbbfb
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2023-10-19 09:51:09 +0200

    appease linter

--- lib/src/test/java/com/dynatrace/metric/util/NormalizationWarnThenDebugLoggerTest.java
+++ lib/src/test/java/com/dynatrace/metric/util/NormalizationWarnThenDebugLoggerTest.java
@@ -11,7 +11,6 @@
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.dynatrace.metric.util;
 
 import static org.mockito.Mockito.*;
